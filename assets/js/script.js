a684d65e00e24fbb37544967d6e9846ee053ba63
EdyDaSilva <ednedson@hotmail.com>
2023-09-15 12:52:14 +0100

    js first const and function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log("Testing..")
+const formEl = document.querySelector(".form");
+
+const inputEl = document.querySelector(".input");
+
+const ulEl = document.querySelector(".list");
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    toDoList();
+});
+
+function toDoList(){
+    let newWhatToDo = inputEl.value;
+    const liEl = document.createElement("li");
+    liEl.innerText = newWhatToDo;
+    ulEl.appendChild(liEl);
+    inputEl.value = ""
+    const checkBtnEl = document.createElement("div");
+    checkBtnEl.innerHTML = `
+    <i class="fa-sharp fa-solid fa-circle-check">
+    `;
+    liEl.appendChild(checkBtnEl);
+    const trashBtnEl = document.createElement("div");
+    trashBtnEl.innerHTML = `
+    <i class="fa-solid fa-trash">
+    `;
+    liEl.appendChild(trashBtnEl);
+}
